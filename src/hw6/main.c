e6d433d6cabc7c7475811f9c2421cfd573111ffc
Катя <k_tupikova00@mail.ru>
2025-11-18 09:25:35 +0300

    Sort list (#8)
    * Add a sorted list
    
    * Modified a sorted list
    
    * Modified pop() in sortList.c, print in main.c
    
    * Use clang-format
    
    * Reusing clang-format
    
    * Add pragma once and replace define checkError

---
+++ src/hw6/main.c
@@ -0,0 +1,35 @@
+#include "sortList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print(list* l)
+{
+    elem_t* curr = l->head;
+    while (curr != NULL) {
+        printf("%d ", *(curr->p));
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    list l;
+    init(&l);
+    int action;
+    scanf("%d", &action);
+    while (action != 0) {
+        int x;
+        if (action == 1) {
+            scanf("%d", &x);
+            checkError(push(&l, x));
+        } else if (action == 2) {
+            scanf("%d", &x);
+            checkError(pop(&l, x));
+        } else
+            print(&l);
+        scanf("%d", &action);
+    }
+    destroy(&l);
+    return 0;
+}
