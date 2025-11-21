0fd5565d03338fc807cbb5d4e34418fc3d42a538
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-21 19:10:26 +0300

    clang-format on bracketBalance

--- src/hw9/bracketBalance/bracketBalance.c
+++ src/hw9/bracketBalance/bracketBalance.c
@@ -2,57 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bracketToNum(char bracket)
-{
-    if (bracket == '(')
-	return 1;
-    if (bracket == '[')
-	return 2;
-    if (bracket == '{')
-	return 3;
-    if (bracket == ')')
-	return 4;
-    if (bracket == ']')
-	return 5;
-    if (bracket == '}')
-	return 6;
-    return -1;
+int bracketToNum(char bracket) {
+  if (bracket == '(')
+    return 1;
+  if (bracket == '[')
+    return 2;
+  if (bracket == '{')
+    return 3;
+  if (bracket == ')')
+    return 4;
+  if (bracket == ']')
+    return 5;
+  if (bracket == '}')
+    return 6;
+  return -1;
 }
 
-int main()
-{
-    stack s;
-    init(&s);
+int main() {
+  stack s;
+  init(&s);
 
-    char bracket = getchar();
-    int answer = 1;
-    
-    while (bracket != '\n') {
+  char bracket = getchar();
+  int answer = 1;
 
-	int num = bracketToNum(bracket);
+  while (bracket != '\n') {
 
-	if (num < 4)
-	    push(&s, num);
-	else {
+    int num = bracketToNum(bracket);
 
-	    if (s.len == 0 || get(&s) != num - 3)
-		answer = 0;
-	    else
-		pop(&s);
-	}
-	
-	bracket = getchar();
+    if (num < 4)
+      push(&s, num);
+    else {
+
+      if (s.len == 0 || get(&s) != num - 3)
+        answer = 0;
+      else
+        pop(&s);
     }
 
-    if (s.len > 0)
-	answer = 0;
-    
-    
-    if (answer)
-	printf("Correct!");
-    else
-	printf("Wrong!");
+    bracket = getchar();
+  }
 
-    destroy(&s);
-    return 0;
+  if (s.len > 0)
+    answer = 0;
+
+  if (answer)
+    printf("Correct!");
+  else
+    printf("Wrong!");
+
+  destroy(&s);
+  return 0;
 }
