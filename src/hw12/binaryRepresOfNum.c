b6cb70495d844c727d29ca28be63ddcbd8882389
Katya Tupikova <k_tupikova00@mail.ru>
2025-11-29 15:48:01 +0300

    use clang-format

--- src/hw12/binaryRepresOfNum.c
+++ src/hw12/binaryRepresOfNum.c
@@ -1,5 +1,5 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void toBin(int dec, int* bin)
 {
@@ -10,20 +10,18 @@ void toBin(int dec, int* bin)
     */
     int swap = 0, i = 0;
     if (dec < 0) {
-	dec = -dec - 1;
-	while (dec > 0)
-	{
-	    bin[i++] = 1 - dec % 2;
-	    dec /= 2;
-	}
-	for (int j = i; j < 32; ++j)
-	    bin[j] = 1;
-	return;
+        dec = -dec - 1;
+        while (dec > 0) {
+            bin[i++] = 1 - dec % 2;
+            dec /= 2;
+        }
+        for (int j = i; j < 32; ++j)
+            bin[j] = 1;
+        return;
     }
-    while (dec > 0)
-    {
-	bin[i++] = dec % 2;
-	dec /= 2;
+    while (dec > 0) {
+        bin[i++] = dec % 2;
+        dec /= 2;
     }
 }
 
@@ -31,9 +29,9 @@ void add(int* a, int* b, int* s)
 {
     int k = 0;
     for (int i = 0; i < 32; ++i) {
-	k = a[i] + b[i] + k;
-	s[i] = k & 1;
-	k >>= 1;
+        k = a[i] + b[i] + k;
+        s[i] = k & 1;
+        k >>= 1;
     }
 }
 
@@ -50,7 +48,7 @@ void printBin(int* bin)
     printf("\n");
     */
     for (int i = 31; i >= 0; --i)
-	printf("%d", bin[i]);
+        printf("%d", bin[i]);
     printf("\n");
 }
 
@@ -58,13 +56,13 @@ int toDec(int* bin)
 {
     int dec = 0;
     if (bin[31]) {
-	dec += 1;
-	for (int i = 0; i < 31; ++i)
-	    dec += ((1 - bin[i]) << i);
-	return -dec;
+        dec += 1;
+        for (int i = 0; i < 31; ++i)
+            dec += ((1 - bin[i]) << i);
+        return -dec;
     }
     for (int i = 0; i < 31; ++i)
-	dec += (bin[i] << i);
+        dec += (bin[i] << i);
     return dec;
 }
 
@@ -72,13 +70,13 @@ int main(void)
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    int binA[32] = {0};
-    int binB[32] = {0};
+    int binA[32] = { 0 };
+    int binB[32] = { 0 };
 
     toBin(a, binA);
     toBin(b, binB);
 
-    int binS[32] = {0};
+    int binS[32] = { 0 };
     add(binA, binB, binS);
 
     printBin(binA);
