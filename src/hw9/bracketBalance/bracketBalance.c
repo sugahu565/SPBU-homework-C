94c4b012adaae3bd6261838592743ffa85c5dae1
Катя <k_tupikova00@mail.ru>
2025-12-04 22:22:07 +0300

    Ci (#12)
    * add hw5
    
    * Add cmake files
    
    * add cmake in src
    
    * all executable files in bin
    
    * clang-format on bracketBalance
    
    * Add clang-format and clang-tidy
    
    * change clang-format, add web-kit
    
    * add dependabot
    
    * add build and lint
    
    * use clang-format
    
    * clang-tidy formatting
    
    * clang-tidy formatting
    
    * use clang-format on bracket balance
    
    * change clang-format
    
    * change cmake in root
    
    * fix clang-tidy name
    
    * delete wrong file from git
    
    * fixed code by clang-tidy

--- src/hw9/bracketBalance/bracketBalance.c
+++ src/hw9/bracketBalance/bracketBalance.c
@@ -2,54 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int bracketToNum(char bracket) {
-  if (bracket == '(')
-    return 1;
-  if (bracket == '[')
-    return 2;
-  if (bracket == '{')
-    return 3;
-  if (bracket == ')')
-    return 4;
-  if (bracket == ']')
-    return 5;
-  if (bracket == '}')
-    return 6;
-  return -1;
+int bracketToNum(char bracket)
+{
+    if (bracket == '(')
+        return 1;
+    if (bracket == '[')
+        return 2;
+    if (bracket == '{')
+        return 3;
+    if (bracket == ')')
+        return 4;
+    if (bracket == ']')
+        return 5;
+    if (bracket == '}')
+        return 6;
+    return -1;
 }
 
-int main() {
-  stack s;
-  init(&s);
+int main(void)
+{
+    stack s;
+    init(&s);
 
-  char bracket = getchar();
-  int answer = 1;
+    char bracket = (char)getchar();
+    int answer = 1;
 
-  while (bracket != '\n') {
+    while (bracket != '\n') {
 
-    int num = bracketToNum(bracket);
+        int num = bracketToNum(bracket);
 
-    if (num < 4)
-      push(&s, num);
-    else {
+        if (num < 4)
+            push(&s, num);
+        else {
 
-      if (s.len == 0 || get(&s) != num - 3)
-        answer = 0;
-      else
-        pop(&s);
+            if (s.len == 0 || get(&s) != num - 3)
+                answer = 0;
+            else
+                pop(&s);
+        }
+
+        bracket = (char)getchar();
     }
 
-    bracket = getchar();
-  }
+    if (s.len > 0)
+        answer = 0;
 
-  if (s.len > 0)
-    answer = 0;
+    if (answer)
+        printf("Correct!");
+    else
+        printf("Wrong!");
 
-  if (answer)
-    printf("Correct!");
-  else
-    printf("Wrong!");
-
-  destroy(&s);
-  return 0;
+    destroy(&s);
+    return 0;
 }
