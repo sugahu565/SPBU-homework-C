8e39332a9d69330be8721fbe1c73bf5db9ca6e5a
Katya Tupikova <k_tupikova00@mail.ru>
2025-12-05 17:03:41 +0300

    use clang format

--- src/controlWork2/task2/main.c
+++ src/controlWork2/task2/main.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void printBin(unsigned int a)
 {
     unsigned int mask = (1 << 31);
     while (mask > 0) {
-	printf("%d", (a & mask) > 0);
-	mask >>= 1;
+        printf("%d", (a & mask) > 0);
+        mask >>= 1;
     }
     printf("\n");
 }
@@ -16,11 +16,11 @@ int getMax(unsigned int num)
     unsigned int maxNum = num;
     int lastBit;
     for (int i = 0; i < 31; ++i) {
-	lastBit = num & 1;
-	num >>= 1;
-	num |= (lastBit << 31);
-	if (maxNum < num)
-	    maxNum = num;
+        lastBit = num & 1;
+        num >>= 1;
+        num |= (lastBit << 31);
+        if (maxNum < num)
+            maxNum = num;
     }
     return maxNum;
 }
