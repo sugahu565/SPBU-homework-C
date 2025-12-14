21590f6529569c4c3fa7c3b13cf9444d217ad8cb
Katya Tupikova <k_tupikova00@mail.ru>
2025-12-14 14:02:15 +0300

    use clang-format webkit

--- src/hw4/main.c
+++ src/hw4/main.c
@@ -2,36 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  int *arr[100];
-  int *arr[100];
-  if (arr == NULL || arrCopy == NULL)
-    return -1;
-  printf("Write an array (enter is the end): ");
+int main()
+{
+    int* arr[100];
+    int* arr[100];
+    if (arr == NULL || arrCopy == NULL)
+        return -1;
+    printf("Write an array (enter is the end): ");
 
-  int elem, len = 0, flag = 1;
-  char c;
+    int elem, len = 0, flag = 1;
+    char c;
 
-  while (flag) {
-    scanf("%d", &elem);
-    arr[len] = elem;
-    arrCopy[len] = elem;
-    ++len;
-    if (getchar() == '\n')
-      flag = 0;
-  }
-  sort(arr, arr + len);
+    while (flag) {
+        scanf("%d", &elem);
+        arr[len] = elem;
+        arrCopy[len] = elem;
+        ++len;
+        if (getchar() == '\n')
+            flag = 0;
+    }
+    sort(arr, arr + len);
 
-  int changePositions = 0, now = 0;
+    int changePositions = 0, now = 0;
 
-  while (now < len) {
-    printf("%d ", arr[now]);
-    if (arr[now] != arrCopy[now])
-      ++changePositions;
-    now++;
-  }
-  printf("\n");
-  free(arr);
-  free(arrCopy);
-  return changePositions;
+    while (now < len) {
+        printf("%d ", arr[now]);
+        if (arr[now] != arrCopy[now])
+            ++changePositions;
+        now++;
+    }
+    printf("\n");
+    free(arr);
+    free(arrCopy);
+    return changePositions;
 }
