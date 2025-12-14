f6f82cffc430486cfee81d6f6e5a7a164e770cbb
Katya Tupikova <k_tupikova00@mail.ru>
2025-12-14 14:05:09 +0300

    use clang-format on sort.c

--- src/hw4/sort.c
+++ src/hw4/sort.c
@@ -4,11 +4,11 @@ int a[MAX_SIZE];
 void sort(int* startArr, int* endArr)
 {
     if (startArr + 1 >= endArr)
-	return;
+        return;
 
     int diff = (endArr - startArr) / 2;
     int* mid = startArr + diff;
-    
+
     sort(startArr, mid);
     sort(mid, endArr);
 
@@ -17,33 +17,33 @@ void sort(int* startArr, int* endArr)
     int* j = mid;
 
     while (i < mid && j < endArr) {
-	if (*i < *j) {
-	    *now = *i;
-	    ++i;
-	} else {
-	    *now = *j;
-	    ++j;
-	}
-	++now;
+        if (*i < *j) {
+            *now = *i;
+            ++i;
+        } else {
+            *now = *j;
+            ++j;
+        }
+        ++now;
     }
 
     while (i < mid) {
-	*now = *i;
-	++now;
-	++i;
+        *now = *i;
+        ++now;
+        ++i;
     }
 
     while (j < endArr) {
-	*now = *j;
-	++now;
-	++j;
+        *now = *j;
+        ++now;
+        ++j;
     }
 
     now = a;
 
     while (startArr < endArr) {
-	*startArr = *now;
-	++startArr;
-	++now;
+        *startArr = *now;
+        ++startArr;
+        ++now;
     }
 }
