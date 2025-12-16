418df1eee2276a5ee8d0cd36b649ac8ad82ecab0
Katya Tupikova <k_tupikova00@mail.ru>
2025-12-17 02:24:41 +0300

    use clang-format

--- src/hw6/cycleList.h
+++ src/hw6/cycleList.h
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 
 typedef struct elem {
-    int* p;
-    struct elem* next;
+  int *p;
+  struct elem *next;
 } elem_t;
 
 typedef struct {
-    int len;
-    elem_t* head;
+  int len;
+  elem_t *head;
 } cycleList;
 
-void init(cycleList* l);
-int push(cycleList* l, elem_t* prevElem, int x);
-void pop(cycleList* l, elem_t* prevElem);
-void destroy(cycleList* l);
+void init(cycleList *l);
+int push(cycleList *l, elem_t *prevElem, int x);
+void pop(cycleList *l, elem_t *prevElem);
+void destroy(cycleList *l);
