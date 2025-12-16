e74f9f6636e4cc49b45a55fbb663bf180ab847e6
Katya Tupikova <k_tupikova00@mail.ru>
2025-12-17 02:26:32 +0300

    use clang-format webkit

--- src/hw6/cycleList.c
+++ src/hw6/cycleList.c
@@ -2,52 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void init(cycleList *l) {
-  l->head = NULL;
-  l->len = 0;
-}
-
-int push(cycleList *l, elem_t *prevElem, int x) {
-  elem_t *new = malloc(sizeof(elem_t));
-  if (!new)
-    return -1;
-
-  new->p = malloc(sizeof(int));
-  if (new->p == NULL) {
-    free(new);
-    return -1;
-  }
-  *(new->p) = x;
-  l->len++;
-  if (l->head == NULL) {
-    l->head = new;
-    new->next = new;
-    return 0;
-  }
-  new->next = prevElem->next;
-  prevElem->next = new;
-  return 0;
-}
-
-void pop(cycleList *l, elem_t *prevElem) {
-  // delete elem after prevElem;
-  l->len--;
-  if (prevElem->next == prevElem) {
-    free(prevElem->p);
-    free(prevElem);
+void init(cycleList* l)
+{
     l->head = NULL;
-    return;
-  }
-  elem_t *needToPop = prevElem->next;
-  prevElem->next = prevElem->next->next;
-  if (l->head == needToPop)
-    l->head = prevElem->next;
-  free(needToPop->p);
-  free(needToPop);
+    l->len = 0;
 }
 
-void destroy(cycleList *l) {
-  while (l->len != 0) {
-    pop(l, l->head);
-  }
+int push(cycleList* l, elem_t* prevElem, int x)
+{
+    elem_t* new = malloc(sizeof(elem_t));
+    if (!new)
+        return -1;
+
+    new->p = malloc(sizeof(int));
+    if (new->p == NULL) {
+        free(new);
+        return -1;
+    }
+    *(new->p) = x;
+    l->len++;
+    if (l->head == NULL) {
+        l->head = new;
+        new->next = new;
+        return 0;
+    }
+    new->next = prevElem->next;
+    prevElem->next = new;
+    return 0;
+}
+
+void pop(cycleList* l, elem_t* prevElem)
+{
+    // delete elem after prevElem;
+    l->len--;
+    if (prevElem->next == prevElem) {
+        free(prevElem->p);
+        free(prevElem);
+        l->head = NULL;
+        return;
+    }
+    elem_t* needToPop = prevElem->next;
+    prevElem->next = prevElem->next->next;
+    if (l->head == needToPop)
+        l->head = prevElem->next;
+    free(needToPop->p);
+    free(needToPop);
+}
+
+void destroy(cycleList* l)
+{
+    while (l->len != 0) {
+        pop(l, l->head);
+    }
 }
